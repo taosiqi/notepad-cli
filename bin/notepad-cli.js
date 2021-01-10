d62f6cf4c4b5dc922dcc81769cd82e7b7f652d69
陶思淇 <1870894297@qq.com>
2021-01-10 19:37:55 +0800

    添加注释

--- bin/notepad-cli.js
+++ bin/notepad-cli.js
@@ -4,7 +4,7 @@ const Service = require('../lib/service')
 const service = new Service()
 
 const rawArgv = process.argv.slice(2)
-const args = require('minimist')(rawArgv)
+const args = require('minimist')(rawArgv) //解析命令行参数
 const command = args._[0]
-
+// 执行初始化
 service.run(command, args, rawArgv)
